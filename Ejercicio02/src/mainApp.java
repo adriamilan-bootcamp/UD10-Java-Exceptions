99b5e51468340596976cdc0a0590f16785f851ef
Celestinvib <celestinvib@gmail.com>
2022-07-18 13:51:01 +0200

    Ex2 mainApp v1.0

--- Ejercicio02/src/mainApp.java
+++ Ejercicio02/src/mainApp.java
@@ -4,11 +4,25 @@ import customException.CustomException;
 public class mainApp {
 	
 	public static void main(String[] args) {
-		
+		inputOutput();
 	}
 	
+	
+	/*
+	 * Metodo que muestra mensahes y crea un objeto de la clase exception
+	 */
 	public static void inputOutput() {
 		
+		try {
+			System.out.println("Mensaje mostrado por pantalla");
+			
+			throw new CustomException("Execepcion captura por mesnsaje: Esto es un objeto Excepcion");
+		}catch (CustomException ex) {
+			System.out.println(ex);
+		}
+		finally {
+			System.out.println("Programa terminado");
+		}
 	}
 	
 }
