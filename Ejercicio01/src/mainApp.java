f396c6c2fdfefccdd74be7f2ec47819f8a647826
Celestinvib <celestinvib@gmail.com>
2022-07-18 12:07:01 +0200

    Ex1 Clase mainApp v0.2 adivinarNumero() hecho

--- Ejercicio01/src/mainApp.java
+++ Ejercicio01/src/mainApp.java
@@ -1,6 +1,42 @@
+import java.io.IOException;
+import java.util.InputMismatchException;
+
+import javax.swing.JOptionPane;
 
 public class mainApp {
+	
 	public static void main(String[] args) {
+	
+	}
+	
+	/*
+	 * Metodo que recibe un numero aleatorio y hace que el usuario introduzca numeros hasta adivinarlo.
+	 * @param numero aleatorio recibido.
+	 */
+	public static void adivinarNumero(NumeroAleatorio numAleatorio) {	
 		
+		int userNum = Integer.MIN_VALUE;
+		int intentos = 0;
+				
+		while(userNum!= numAleatorio.getValor()) {
+			try {
+				userNum = Integer.parseInt(JOptionPane.showInputDialog(null,"[Adivina el numero secreto]\nIntroduce un numero:"));
+				
+				if(userNum > numAleatorio.getValor()) {
+					JOptionPane.showMessageDialog(null,"El numero que tienes que adivinar es menor al que has introducido");
+				}else if (userNum < numAleatorio.getValor()) {
+					JOptionPane.showMessageDialog(null,"El numero que tienes que adivinar es mayor al que has introducido");				
+				}
+				
+				intentos++;
+		
+			}catch (InputMismatchException e){
+				System.out.println(e);
+			}catch (Exception e) {
+				System.out.println(e);
+			}
+		}
+		
+		JOptionPane.showMessageDialog(null,"¡Has adivinado el numero en "+intentos+"  intentos!, el numero secreto era "+userNum);				
 	}
 }
