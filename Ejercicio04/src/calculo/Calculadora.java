bef361bd45f2bd5abc4c237f23ed99613397c3bf
Celestinvib <celestinvib@gmail.com>
2022-07-18 17:39:50 +0200

    ex4 Calculadora v0.1 (Faltan metodos y comentarios)

--- Ejercicio04/src/calculo/Calculadora.java
+++ Ejercicio04/src/calculo/Calculadora.java
@@ -1,5 +1,51 @@
 package calculo;
 
-public class Calculadora {
+public class Calculadora { 
+	
+	/* Atributos */
+	private boolean encendida;
+	
+	private double resultado;
+
+	/**
+	 * Constructor por defecto
+	 */
+	public Calculadora() {
+		this.encendida = true;
+	}
+
+	/* Getters y Setters */
+
+	/**
+	 * @return the encendida
+	 */
+	public boolean isEncendida() {
+		return encendida;
+	}
+
+	/**
+	 * @param encendida the encendida to set
+	 */
+	public void setEncendida(boolean encendida) {
+		this.encendida = encendida;
+	}
+
+	/**
+	 * @return the resultado
+	 */
+	public double getResultado() {
+		return resultado;
+	}
+	
+    //Metodo de ejemplo (Realizar comentarios despues)	
+	public void suma(int num1, int num2) {
+		this.resultado =  num1 + num2;  
+    }
+	
+
+	
+	
+	
+	
 
 }
