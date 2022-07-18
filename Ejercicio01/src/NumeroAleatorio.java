dbb0c961aff166493fb55114cdcb2b351faec70f
Adrià Milan <adria4milan@gmail.com>
2022-07-18 11:01:15 +0200

    Exercice01 Metodos

--- Ejercicio01/src/NumeroAleatorio.java
+++ Ejercicio01/src/NumeroAleatorio.java
@@ -1,4 +1,12 @@
 
 public class NumeroAleatorio {
 
+	public int generarNumero(int max, int min) {
+		return (int) (Math.random() * (max - min)) + min;
+	}
+
+	public void adivinarNumero() {
+		
+	}
+	
 }
