95866de44f2d05d5fa33e80063a0efe58f3e8caf
Adrià Milan <adria4milan@gmail.com>
2022-07-19 22:49:56 +0200

    Ej04 Calculadora v1.1

--- Ejercicio04/src/calculo/Calculadora.java
+++ Ejercicio04/src/calculo/Calculadora.java
@@ -3,49 +3,103 @@ package calculo;
 public class Calculadora { 
 	
 	/* Atributos */
-	private boolean encendida;
-	
-	private double resultado;
+	private double numero1 = 0.00;
+	private double numero2 = 0.00;
 
-	/**
-	 * Constructor por defecto
+	/*
+	 * Constructor para:
+	 * - Raiz cuadrada
+	 * - Raiz cubica
 	 */
-	public Calculadora() {
-		this.encendida = true;
-	}
-
-	/* Getters y Setters */
-
-	/**
-	 * @return the encendida
-	 */
-	public boolean isEncendida() {
-		return encendida;
-	}
-
-	/**
-	 * @param encendida the encendida to set
-	 */
-	public void setEncendida(boolean encendida) {
-		this.encendida = encendida;
-	}
-
-	/**
-	 * @return the resultado
-	 */
-	public double getResultado() {
-		return resultado;
+	public Calculadora(double valor1, String operacion) {
+		this.numero1 = valor1;
 	}
 	
-    //Metodo de ejemplo (Realizar comentarios despues)	
-	public void suma(int num1, int num2) {
-		this.resultado =  num1 + num2;  
+	/*
+	 * Constructor para:
+	 * - Suma
+	 * - Resta
+	 * - Multiplicacion
+	 * - Division
+	 * - Potencia
+	 */
+	public Calculadora(int valor1, double valor2) {
+		this.numero1 = valor1;
+		this.numero2 = valor2;
+	}
+	
+	// Getters
+
+    /**
+	 * @return the numero1
+	 */
+	public double getNumero1() {
+		return numero1;
+	}
+
+	/**
+	 * @return the numero2
+	 */
+	public double getNumero2() {
+		return numero2;
+	}
+
+	/*
+     * Metodo suma
+     * numero1 + numero2	
+     */
+	public double suma() {
+		return this.numero1 + this.numero2;
     }
 	
-
+	/*
+     * Metodo multiplicar
+     * numero1 * numero2	
+     */
+	public double multiplicar() {
+		return this.numero1 * this.numero2;
+    }
 	
+	/*
+     * Metodo potencia
+     * numero1^numero2	
+     */
+	public double potencia() {
+		double resultado = 0.00;
+		for(int i = 0; i < this.numero2; i++) {
+			resultado *= this.numero1; 
+		}
+		return resultado;
+    }
 	
+	/*
+     * Metodo dividir
+     * numero1 / numero2	
+     */
+	public double dividir() {
+		return this.numero1 / this.numero2;
+    }
 	
+	/*
+     * Metodo restar
+     * numero1 - numero2	
+     */
+	public double restar() {
+		return this.numero1 - this.numero2;
+    }
 	
-
+	/*
+     * Metodo raiz cuadrada
+     * √numero1
+     */
+	public double raizCuadrada() {
+		return Math.sqrt(this.numero1);
+    }
+	
+	/*
+	 * Metodo raiz cubica
+	 */
+	public double raizCubica() {
+		return Math.cbrt(this.numero1);
+	}
 }
