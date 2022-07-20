577277091a17311799d296c30d3da21ff8854472
Celestinvib <celestinvib@gmail.com>
2022-07-20 20:55:43 +0200

    Ex3 nAleatorio v0.2 Comentarios added

--- Ejercicio03/src/nAleatorio.java
+++ Ejercicio03/src/nAleatorio.java
@@ -2,27 +2,40 @@ import java.util.Random;
 
 public class nAleatorio {
 	
+	/* Atributo */	
 	private int numero;
 	
+	/* Constructor */
 	public nAleatorio() {
-		// TODO Auto-generated constructor stub
-		System.out.println("Generando numero aleatorio...");
+//		System.out.println("Generando numero aleatorio...");
 		genNum();
-		System.out.println("El numero generado es: "+numero);
-		esPar();
+	}
+		
+	/* Getter */
+	/**
+	 * @return the numero
+	 */
+	public int getNumero() {
+		return numero;
+	}
+
+	/*
+     * Metodo que genera aleatoriamente un numero entre 0 y 999
+     */
+	public void genNum() {
+		Random r = new Random();
+		this.numero =  r.nextInt(999); 
 	}
 	
+	/*
+     * Metodo comprueba si el numero generado aleatoriamente es par o impar
+     */
 	public void esPar() {
-		if(numero%2 == 0) {
+		if(numero % 2 == 0) {
 			System.out.println("Es par");
 			return;
 		}
 		System.out.println("Es impar");
-	}
-	
-	public void genNum() {
-		Random r = new Random();
-		this.numero =  r.nextInt(999); 
 	}
 
 }
