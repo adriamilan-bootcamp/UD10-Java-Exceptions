294759abecf88b02b0e5e85616abc86b3742331e
Celestinvib <celestinvib@gmail.com>
2022-07-20 20:56:08 +0200

    Ex3 mainApp v0.2

--- Ejercicio03/src/mainApp.java
+++ Ejercicio03/src/mainApp.java
@@ -2,5 +2,6 @@
 public class mainApp {
 	public static void main(String[] args) {
 		nAleatorio ale = new nAleatorio();
+		//Se tiene que (mostar los mensaje desde el main  lanzando la excepcion 
 	}
 }
